7f81cf4f101d05397f338be28fd1f8ea613fa2ed
Matthew Mincher <matthew@mincher.org>
2021-11-04 23:17:57 +0000

    Continuing with deployment (mysql incompatibility - get a local instance set up)

--- database/migrations/2021_10_19_232044_add_author_to_blog_posts_table.php
+++ database/migrations/2021_10_19_232044_add_author_to_blog_posts_table.php
@@ -14,7 +14,7 @@ class AddAuthorToBlogPostsTable extends Migration
     public function up()
     {
         Schema::table('blog_posts', function (Blueprint $table) {
-            $table->unsignedInteger('user_id')->default(0);
+            $table->unsignedBigInteger('user_id')->default(0);
 
             $table->foreign('user_id')->references('id')->on('users');
         });
---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'blog.matthewmincher.dev');
+
+// Project repository
+set('repository', 'https://github.com/matthewmincher/blog.matthewmincher.dev.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('blog.matthewmincher.dev')
+    ->set('deploy_path', '/var/www/{{application}}')
+    ->multiplexing(false);
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
